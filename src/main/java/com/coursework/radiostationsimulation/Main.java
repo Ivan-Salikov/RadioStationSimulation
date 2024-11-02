b0790a57581636bb461c6789c448d89e648b1819
Ivan-Saikov <salikovivan7@gmail.com>
2024-11-02 19:12:57 +0500

    Создана ветка Dev. Начата реализация работы с фонотекой

--- src/main/java/com/coursework/radiostationsimulation/Main.java
+++ src/main/java/com/coursework/radiostationsimulation/Main.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("SimulationForm.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MusicLibraryForm.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1235, 570);
         stage.setTitle("Модель составления программ радиостанции");
         stage.setScene(scene);
